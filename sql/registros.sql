41744e7a85ad0aa203bb50d918a2e27a7575f1ee
JRodriAlarc <jona10131313@gmail.com>
2024-05-01 17:34:05 -0600

    Ultimo respaldo de la Base de Datos

--- sql/registros.sql
+++ sql/registros.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-02-2023 a las 02:25:17
--- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.6
+-- Tiempo de generación: 02-05-2024 a las 01:31:08
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,21 +34,22 @@ CREATE TABLE `registros` (
   `telefono` char(10) DEFAULT NULL COMMENT 'phone',
   `contrasenia` varchar(30) DEFAULT NULL COMMENT 'password',
   `rol` varchar(13) DEFAULT NULL COMMENT 'rol',
-  `lectura` tinyint(1) DEFAULT NULL COMMENT 'ver',
-  `escritura` tinyint(1) DEFAULT NULL COMMENT 'editar',
-  `status` tinyint(1) DEFAULT NULL COMMENT 'estado'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Almacena Datos de Usuarios';
+  `lectura` char(2) DEFAULT NULL COMMENT 'ver',
+  `escritura` char(2) DEFAULT NULL COMMENT 'editar',
+  `status` char(2) DEFAULT NULL COMMENT 'estado'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Almacena Datos de Usuarios';
 
 --
 -- Volcado de datos para la tabla `registros`
 --
 
 INSERT INTO `registros` (`id`, `nombre`, `email`, `telefono`, `contrasenia`, `rol`, `lectura`, `escritura`, `status`) VALUES
-(1, 'John Smith', 'moydutogno@gufum.com', '7714981574', 'qwerty000', 'Cliente', "No", "Si", "Si"),
-(2, 'Jona Rodri', 'JonaRod@gmail.com', '7711460824', 'qwerty000', 'Administrador', "Si", "Si", "Si"),
-(3, 'Juan Perez', 'JuanPer3z@gmail.com', '7713751637', 'qwerty000', 'Vendedor', "No", "No", "Si"),
-(4, 'Tomas Gómez', 'Tom45@edu.mx', '7715623943', 'qwerty000', 'Cliente', "Si", "Si", "Si"),
-(5, 'Fernando', 'Frenan145@gmail.com', '7716453689', 'qwerty000', 'Vendedor', "Si", "No", "Si");
+(1, 'John Smith', 'moydutogno@gufum.com', '7712983217', 'qwerty000', 'Cliente', 'No', 'No', 'Si'),
+(2, 'Jona Rodri', 'JonaRod@gmail.com', '7711460284', 'qwerty000', 'Administrador', 'Si', 'Si', 'Si'),
+(3, 'Juan Perez', 'JuanPer3z@gmail.com', '7713751532', 'qwerty000', 'Vendedor', 'No', 'Si', 'Si'),
+(4, 'Juan Gomez', 'juanGomez@mail.com', '7715487595', 'qwerty000', 'Cliente', 'Si', 'No', 'No'),
+(5, 'Carlos García', 'carlosGarza@mail.com', '7715487595', 'qwerty000', 'Vendedor', 'Si', 'No', 'Si'),
+(6, 'Rosa', 'rosahernan@mail.com', '7713217678', 'qwerty000', 'Administrador', 'Si', 'No', 'Si');
 
 --
 -- Índices para tablas volcadas
@@ -68,7 +69,7 @@ ALTER TABLE `registros`
 -- AUTO_INCREMENT de la tabla `registros`
 --
 ALTER TABLE `registros`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Primary Key', AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Primary Key', AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
